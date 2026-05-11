1fae2a7bd22d67469ed5c558fe546944a8856c4b
Zwiebel56 <ezequielzwiebel@gmail.com>
2026-05-11 12:09:45 -0300

    Initial commit

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    if(Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if(transform.position.x < 8f)
+            {
+                transform.Translate(2f,0,0);
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if(transform.position.x > -8f)
+            {
+                transform.Translate(-2f,0,0);
+            }
+        }
+    }
+}
