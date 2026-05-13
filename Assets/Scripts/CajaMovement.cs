af94f1f3bd92d7f7072c2ca82f7806fb70836013
Zwiebel56 <ezequielzwiebel@gmail.com>
2026-05-13 10:43:01 -0300

    Terminado

--- Assets/Scripts/CajaMovement.cs
+++ Assets/Scripts/CajaMovement.cs
@@ -1,19 +1,59 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
+ 
 public class CajaMovement : MonoBehaviour
 {
-    public float speed;
-    // Start is called before the first frame update
+    public float velocidadCaida = 3f;
+    public float incrementoVelocidad = 1f;
+    public TextMeshProUGUI tiempoTexto;
+ 
+    private float tiempoTranscurrido = 0f;
+    private bool juegoActivo = true;
+    private Rigidbody rb;
+ 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.freezeRotation = true;
     }
-
-    // Update is called once per frame
+ 
     void Update()
     {
-        transform.Translate(0,-speed * Time.deltaTime,0);
+        if (!juegoActivo) return;
+ 
+        // Caida recta hacia abajo
+        transform.position += Vector3.down * velocidadCaida * Time.deltaTime;
+ 
+        // Tiempo en pantalla
+        tiempoTranscurrido += Time.deltaTime;
+        if (tiempoTexto != null)
+            tiempoTexto.text = "Tiempo: " + tiempoTranscurrido.ToString("F2");
+    }
+ 
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.CompareTag("Piso"))
+        {
+            velocidadCaida += incrementoVelocidad;
+ 
+            float x = Random.Range(-8f, 8f);
+            transform.position = new Vector3(x, 8f, 0f);
+ 
+            rb.velocity = Vector3.zero;
+        }
+ 
+        if (col.gameObject.CompareTag("Player"))
+        {
+            juegoActivo = false;
+            rb.velocity = Vector3.zero;
+ 
+            if (tiempoTexto != null)
+                tiempoTexto.text = "¡FIN! Tiempo: " + tiempoTranscurrido.ToString("F2");
+ 
+            Destroy(col.gameObject);
+        }
     }
 }
